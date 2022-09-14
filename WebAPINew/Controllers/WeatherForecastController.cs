c2da2c046ed14a40f608f636cd4a5a71260b7ad1
Garvit <garvit_gupta@hcl.com>
2022-09-14 11:18:17 +0530

    Changes in Weatherforcast

--- WebAPINew/Controllers/WeatherForecastController.cs
+++ WebAPINew/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebAPINew.Controllers
     {        
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
